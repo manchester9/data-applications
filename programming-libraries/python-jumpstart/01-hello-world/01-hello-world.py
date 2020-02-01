26be81849bd4b0c47977f0be3c8961b9a8cb95ef
Roland Ferrao <rolandferrao@yahoo.com>
2020-02-01 15:35:09 -0500

    Add programming libraries?

---
+++ programming-libraries/python-jumpstart/01-hello-world/01-hello-world.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+import numpy as np 
+
+print('--------------------------------')
+print('          HELLO APP')
+print('--------------------------------')
+print()
+
+user_text = input('What is your name? ')
+greeting = 'Nice to meet you ' + user_text
+
+print(greeting)
